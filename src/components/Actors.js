2c10e019c47ad88e875d03f330a70f6272babf37
smassarsky <seth.massarsky@gmail.com>
2021-02-15 17:41:53 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,9 +4,23 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors()}
     </div>
   );
 };
+
+const renderActors = () => {
+  return actors.map(actor => {
+    return (
+      <div>
+        <h3>{actor.name}</h3>
+        <ul>
+          {actor.movies.map(movie => <li>{movie}</li>)}
+        </ul>
+      </div>
+    )
+  })
+}
 
 export default Actors;
